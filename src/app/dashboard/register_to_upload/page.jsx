cd269665eb4db9eac93ef33597c4f0a9c65542d9
rash <ashar.shakil04@gmail.com>
2024-03-27 12:51:02 +0530

    Table page addition

---
+++ src/app/dashboard/register_to_upload/page.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import styles from "@/ui/dashboard/table/table.module.css"
+import Pagination from '@/ui/pagination/pagination'
+
+const RegisterPage = () => {
+  return (
+    <div className={styles.mainContainer}>
+        <h2 className={styles.title}>Application to Register to Upload</h2>
+    <div class={styles.container}>
+        <div className={styles.topBar}>
+            Search element...
+        </div>
+	<table className={styles.table}>
+		<thead>
+			<tr>
+				<th>Case Reference #</th>
+				<th>Client Name</th>
+				<th>FE Name</th>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody className={styles.content}>
+			<tr>
+				<td>Cell 1</td>
+				<td>Cell 2</td>
+				<td>Cell 3</td>
+				<td>
+                    <button className={styles.button}>View Record</button>
+                </td>
+
+			</tr>
+			<tr>
+				<td>Cell 1</td>
+				<td>Cell 2</td>
+				<td>Cell 3</td>
+				<td><button className={styles.button}>View Record</button></td>
+
+			</tr>
+			<tr>
+				<td>Cell 1</td>
+				<td>Cell 2</td>
+				<td>Cell 3</td>
+				<td><button className={styles.button}>View Record</button></td>
+
+			</tr>
+			<tr>
+				<td>Cell 1</td>
+				<td>Cell 2</td>
+				<td>Cell 3</td>
+				<td><button className={styles.button}>View Record</button></td>
+
+			</tr>
+			<tr>
+				<td>Cell 1</td>
+				<td>Cell 2</td>
+				<td>Cell 3</td>
+				<td><button className={styles.button}>View Record</button></td>
+
+			</tr>
+		</tbody>
+	</table>
+    <div className={styles.botBar}>
+        <Pagination></Pagination>
+    </div>
+</div>
+</div>
+  )
+}
+
+export default RegisterPage
