5765e5d2761a0b422b616e5a874d71fb085db5d3
rash <ashar.shakil04@gmail.com>
2024-04-02 13:30:23 +0530

    CaseInfo updatation

--- src/ui/navbar/navbar.jsx
+++ src/ui/navbar/navbar.jsx
@@ -2,15 +2,16 @@
 import { usePathname } from "next/navigation";
 import styles from "@/ui/navbar/navbar.module.css";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
   const pathname = usePathname();
+  const router = useRouter();
 
   return (
     <div className={styles.container}>
-      <Link href="/dashboard">
-        <button> &lt; &nbsp;Go Back</button>
-      </Link>
+        <button type="button" onClick={router.back}> &lt; &nbsp;Go Back</button>
+      <div className={styles.title}>{pathname.split("/").pop()}</div>
       <Link href="/">
         <button className={styles.logout}>Logout</button>
       </Link>
